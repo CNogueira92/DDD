8e0b19d9b29a4f7e79d79952f9a2a378e420628b
Carlos Nogueira <desenvolvimento@cenariocapital.com.br>
2017-04-03 14:43:35 -0300

    Adicionando arquivos estrutura inical

---
+++ ModeloDDD.Infra.Data/Context/ModeloDDContext.cs
@@ -0,0 +1,61 @@
+﻿using ModeloDDD.Domain.Entities;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace ModeloDDD.Infra.Data.Context 
+{
+    public class ModeloDDContext : DbContext       
+    {
+        public ModeloDDContext()
+             : base("ModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Remove pluralização
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            //Coloca ClietID como Id
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            //Coloca string como varchar e não nvarchar
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            //Coloca tamanho maximo
+            modelBuilder.Properties<string>()
+               .Configure(p => p.HasMaxLength(255));
+
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
+
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+    }
+
+   
+}
